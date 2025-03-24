3bbb9a8674df287e58bcf0473cd70576a0036caa
ORIN-AYO <orinayo2022@gmail.com>
2025-03-24 17:12:16 +0100

    file name complication resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './index.css'
-import Navbar from './components/Navbar/Navbar'
+import Navbar from './Components/Navbar/Navbar'
 import { Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home/Home'
 import Coin from './Pages/Coin/Coin'
